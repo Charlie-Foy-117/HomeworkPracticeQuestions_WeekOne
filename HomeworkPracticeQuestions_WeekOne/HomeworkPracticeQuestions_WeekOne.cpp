821fef5c71882dae1d30d4ef8c696c02836d5a50
Charlie-Foy-117 <83784786+Charlie-Foy-117@users.noreply.github.com>
2022-08-31 17:28:13 +0100

    Problem one area of a rectangle

---
+++ HomeworkPracticeQuestions_WeekOne/HomeworkPracticeQuestions_WeekOne.cpp
@@ -0,0 +1,31 @@
+// HomeworkPracticeQuestions_WeekOne.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    //problem one : Area of a rectangle
+    int length, width, area;
+
+    std::cout << "Please enter the length of the rectangle" << std::endl;
+    std::cin >> length;
+    std::cout << "Please enter the width of the rectangle" << std::endl;
+    std::cin >> width;
+    area = length * width;
+    std::cout << "The area of the rectangle is " << area << std::endl;
+
+    //problem two : Time Conversion
+    
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
